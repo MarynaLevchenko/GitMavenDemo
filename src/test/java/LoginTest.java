6318b706674698c281976e24e9b9eec87f854541
MarynaLevchenko <levchenkomaryna77@gmail.com>
2021-07-14 15:50:12 -0700

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    public static WebDriver driver;
+
+    @BeforeSuite
+    public void setUp(){
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void doLogin(){
+
+        driver.get("http://gmail.com");
+        driver.findElement(By.id("identifierId")).sendKeys("Levchenkomaryna77@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
+
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+}
